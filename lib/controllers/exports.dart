42a9447a6a834a175d2684c76d8d03a662e05b93
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2024-05-14 10:18:14 +0530

    code base setup

---
+++ lib/controllers/exports.dart
@@ -0,0 +1,8 @@
+export 'package:job_sathi/controllers/bookmark_provider.dart';
+export 'package:job_sathi/controllers/image_provider.dart';
+export 'package:job_sathi/controllers/jobs_provider.dart';
+export 'package:job_sathi/controllers/login_provider.dart';
+export 'package:job_sathi/controllers/onboarding_provider.dart';
+export 'package:job_sathi/controllers/profile_provider.dart';
+export 'package:job_sathi/controllers/signup_provider.dart';
+export 'package:job_sathi/controllers/zoom_provider.dart';
---
+++ lib/views/common/exports.dart
@@ -0,0 +1,3 @@
+export 'package:job_sathi/constants/app_constants.dart';
+export 'package:job_sathi/views/common/app_style.dart';
+export 'package:job_sathi/views/common/reusable_text.dart';
