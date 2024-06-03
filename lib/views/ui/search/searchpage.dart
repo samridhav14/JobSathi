214f77e7342814adb8eaa7f672c4430491f94089
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2024-06-03 12:00:53 +0530

    search working properly

--- lib/views/ui/search/searchpage.dart
+++ lib/views/ui/search/searchpage.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:job_sathi/constants/app_constants.dart';
+import 'package:job_sathi/models/response/jobs/jobs_response.dart';
+import 'package:job_sathi/services/helpers/jobs_helper.dart';
+import 'package:job_sathi/views/common/exports.dart';
+import 'package:job_sathi/views/common/vertical_tile.dart';
 import 'package:job_sathi/views/ui/search/widgets/custom_field.dart';
 
 class SearchPage extends StatefulWidget {
@@ -12,7 +16,12 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  TextEditingController controller = TextEditingController();
+  TextEditingController search = TextEditingController();
+  @override
+  void dispose() {
+    search.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +30,7 @@ class _SearchPageState extends State<SearchPage> {
        iconTheme: IconThemeData(color:Color(kLight.value),),
        title:CustomField(
         hintText: 'Search For a Job', 
-        controller: controller,
+        controller: search,
          suffixIcon:GestureDetector(
            onTap: (){
             setState(() {
@@ -33,14 +42,51 @@ class _SearchPageState extends State<SearchPage> {
        ),
       elevation: 0,
       ),
-      body:  Center(
-          child:
-            Padding(padding: EdgeInsets.all(20.h),
-            child:Image.asset('assets/images/optimized_search.png',height: 200.h,width: 200.h,),
-        
+      body: search.text.isNotEmpty?Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder<List<JobsResponse>>(
+          future: JobsHelper.searchJobs(search.text),
+          builder: (context,snapshot){
+            if(snapshot.connectionState==ConnectionState.waiting){
+              return const Center(child: CircularProgressIndicator(),);
+            }
+            else if(snapshot.hasError){
+              return Center(child: ReusableText(text: "Error: ${snapshot.error}", style: appstyle(16, Color(kDark.value), FontWeight.w600),),);
+            }
+            else if(snapshot.data!.isEmpty){
+              return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.h,
+          ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/optimized_search.png"),
+                ReusableText(text: "No Jobs Found", style: appstyle(24, Color(kDark.value), FontWeight.w600),),
+                 ],
             )
-  
-        )
+          );
+            }
+            final jobs = snapshot.data;
+             return ListView.builder(
+              itemCount: jobs!.length,
+              itemBuilder: (context,index){
+                final job = jobs[index];
+                return VerticalTileWidget(jobs: job, onTap: (){});
+              }
+             );
+          },),
+      ) 
+        :Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.h,
+        ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/optimized_search.png"),
+              ReusableText(text: "Start Searching For Jobs", style: appstyle(24, Color(kDark.value), FontWeight.w600),),
+               ],
+          )
+        ),
     );
   }
 }
